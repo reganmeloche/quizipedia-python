5cf18971aa5b616691c6ebccdb99bd55d9716c02
Regan Meloche <reganmeloche@gmail.com>
2020-05-31 18:31:42 -0600

    added post

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask 
-  
+from flask import Flask, request
+from .parser import Parser
+
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+        return "<h1>Welcome!</h1>"
+
+@app.route('/tester', methods=['POST'])
+def tester():
+    p1 = Parser('MyParser')
+    req_data = request.get_json()
+    result = p1.parse(req_data)
+    return result
